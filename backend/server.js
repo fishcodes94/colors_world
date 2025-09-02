37f78c77a861e066f75cbfd60edf8559bbd4e3f8
chadrick <cfishe36@charlotte.edu>
2025-09-02 13:49:58 +0900

    updated origins

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-const allowedOrigins= ["http://localhost:5173"]
+const allowedOrigins= ["http://localhost:5173", "https://colors-world.onrender.com"]
 
 app.use(
   cors({
